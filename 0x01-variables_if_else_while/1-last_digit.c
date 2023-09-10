a2d7307c705812dabbec31f4d9e3ac5d968a60b1
elandas05 <elandas2003@gmail.com>
2023-09-10 16:46:55 -0700

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,8 +20,9 @@ int main(void)
 	if (n > 0)
 		printf("%d is positive", n);
 	else if (n == 0)
-		printf("%d is 0", n);
+		printf("%d is zero", n);
 	else
 		printf("%d is negative", n);
+	printf("\n");
 	return (0);
 }
